72d7357d58b6bdc19e02ae2e7bebadf21157dcc7
Jimmy Nguyen <155388381+guychch@users.noreply.github.com>
2025-03-04 11:36:01 -0800

    Create Practice3-Sentence.java

---
+++ problems/Practice3-Sentence.java
@@ -0,0 +1,39 @@
+ /*
+      this method asks the user to enter a pronoun, a verb and a noun
+      create a sentence of the user input and 
+      return the sentences
+
+      Need to put the code in main method to run. you know what to do.
+   */
+
+      String sentence = getSentence(kb);
+      System.out.print(sentence);
+
+public static String getSentence(Scanner kb)
+   {
+      System.out.print("Enter a pronoun, a verb, an article and a noun : ");
+      String pronoun = kb.next();
+      String verb = kb.next();
+      String article = kb.next();
+      String noun = kb.next();
+      return (pronoun + " " + verb + " " + article + " " + noun);
+   }
+
+/*
+  Another way to do it
+   public static String getSentence(Scanner kb)
+   {
+      String s = "";
+      System.out.print("Enter a pronoun : ")
+       s = s+ kb.next() + " ";;
+      System.out.print("Enter a verb : ")
+       s = s+ kb.next() + " ";
+       System.out.print("Enter a article : ")
+       s = s+ kb.next() + " ";
+       System.out.print("Enter a noun : ")
+       s = s+ kb.next() + " ";
+       return s;
+   }
+
+  */
+
